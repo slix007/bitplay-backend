5c024427a46578c582d629306c6a4c359cac44c2
Sergei Shurmin <slixsl007@gmail.com>
2017-04-07 15:39:42 +0200

    add CORSFilter

--- src/main/java/com/crypto/controller/config/JerseyConfig.java
+++ src/main/java/com/crypto/controller/config/JerseyConfig.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Component;
 public class JerseyConfig extends ResourceConfig {
 
     public JerseyConfig() {
+        register(CORSFilter.class);
         register(BitplayUIEndpoint.class);
     }
 
