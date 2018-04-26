19c3f3b436623394203f586655777b0db57ac20b
Sergei Shurmin <slixsl007@gmail.com>
2018-04-26 22:39:52 +0400

    avg-delta-calc fix empty borderDelta2

--- core/src/main/java/com/bitplay/persistance/DeltaRepositoryService.java
+++ core/src/main/java/com/bitplay/persistance/DeltaRepositoryService.java
@@ -73,6 +73,7 @@ public class DeltaRepositoryService {
         BorderDelta borderDelta = borderParams.getBorderDelta();
         if (borderDelta == null) {
             borderDelta = BorderDelta.createDefault();
+            borderParams.setBorderDelta(borderDelta);
             persistenceService.saveBorderParams(borderParams);
         }
 
