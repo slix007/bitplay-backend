b0809698e7367b40e16308fea4b0d221259761f1
Sergei Shurmin <slixsl007@gmail.com>
2018-09-22 23:02:05 +0400

    okex tick step 0.01

--- core/src/main/java/com/bitplay/persistance/domain/settings/OkexContractType.java
+++ core/src/main/java/com/bitplay/persistance/domain/settings/OkexContractType.java
@@ -13,12 +13,12 @@ import org.knowm.xchange.okcoin.FuturesContract;
 @Getter
 public enum OkexContractType implements ContractType {
 
-    BTC_ThisWeek(FuturesContract.ThisWeek, CurrencyPair.BTC_USD, BigDecimal.valueOf(0.1)),
-    BTC_NextWeek(FuturesContract.NextWeek, CurrencyPair.BTC_USD, BigDecimal.valueOf(0.1)),
-    BTC_Quarter(FuturesContract.Quarter, CurrencyPair.BTC_USD, BigDecimal.valueOf(0.1)),
-    ETH_ThisWeek(FuturesContract.ThisWeek, CurrencyPair.ETH_USD, BigDecimal.valueOf(0.1)),
-    ETH_NextWeek(FuturesContract.NextWeek, CurrencyPair.ETH_USD, BigDecimal.valueOf(0.1)),
-    ETH_Quarter(FuturesContract.Quarter, CurrencyPair.ETH_USD, BigDecimal.valueOf(0.1)),
+    BTC_ThisWeek(FuturesContract.ThisWeek, CurrencyPair.BTC_USD, BigDecimal.valueOf(0.01)),
+    BTC_NextWeek(FuturesContract.NextWeek, CurrencyPair.BTC_USD, BigDecimal.valueOf(0.01)),
+    BTC_Quarter(FuturesContract.Quarter, CurrencyPair.BTC_USD, BigDecimal.valueOf(0.01)),
+    ETH_ThisWeek(FuturesContract.ThisWeek, CurrencyPair.ETH_USD, BigDecimal.valueOf(0.01)),
+    ETH_NextWeek(FuturesContract.NextWeek, CurrencyPair.ETH_USD, BigDecimal.valueOf(0.01)),
+    ETH_Quarter(FuturesContract.Quarter, CurrencyPair.ETH_USD, BigDecimal.valueOf(0.01)),
     ;
 
     private FuturesContract futuresContract;
