af84f86ba5d074e76f3d7ec322e0e4b6d43c64bf
Sergei Shurmin <slixsl007@gmail.com>
2018-10-24 11:48:07 +0400

    rmi - remove code, use startup params

--- core/src/main/java/com/bitplay/ConfigureRMI.java
+++ core/src/main/java/com/bitplay/ConfigureRMI.java
@@ -7,29 +7,30 @@ import org.springframework.context.annotation.DependsOn;
 import org.springframework.jmx.support.ConnectorServerFactoryBean;
 import org.springframework.remoting.rmi.RmiRegistryFactoryBean;
 
-@Configuration
+//@Configuration
+// use startup params -Djava.rmi.server.hostname=664-vuld.fplay.io
 public class ConfigureRMI {
 
-    @Value("${jmx.rmi.host:localhost}")
-    private String rmiHost;
-
-    @Value("${jmx.rmi.port:1099}")
-    private Integer rmiPort;
-
-    @Bean
-    public RmiRegistryFactoryBean rmiRegistry() {
-        final RmiRegistryFactoryBean rmiRegistryFactoryBean = new RmiRegistryFactoryBean();
-        rmiRegistryFactoryBean.setPort(rmiPort);
-        rmiRegistryFactoryBean.setAlwaysCreate(true);
-        return rmiRegistryFactoryBean;
-    }
-
-    @Bean
-    @DependsOn("rmiRegistry")
-    public ConnectorServerFactoryBean connectorServerFactoryBean() throws Exception {
-        final ConnectorServerFactoryBean connectorServerFactoryBean = new ConnectorServerFactoryBean();
-        connectorServerFactoryBean.setObjectName("connector:name=rmi");
-        connectorServerFactoryBean.setServiceUrl(String.format("service:jmx:rmi://%s:%s/jndi/rmi://%s:%s/jmxrmi", rmiHost, rmiPort, rmiHost, rmiPort));
-        return connectorServerFactoryBean;
-    }
+//    @Value("${jmx.rmi.host:localhost}")
+//    private String rmiHost;
+//
+//    @Value("${jmx.rmi.port:1099}")
+//    private Integer rmiPort;
+//
+//    @Bean
+//    public RmiRegistryFactoryBean rmiRegistry() {
+//        final RmiRegistryFactoryBean rmiRegistryFactoryBean = new RmiRegistryFactoryBean();
+//        rmiRegistryFactoryBean.setPort(rmiPort);
+//        rmiRegistryFactoryBean.setAlwaysCreate(true);
+//        return rmiRegistryFactoryBean;
+//    }
+//
+//    @Bean
+//    @DependsOn("rmiRegistry")
+//    public ConnectorServerFactoryBean connectorServerFactoryBean() throws Exception {
+//        final ConnectorServerFactoryBean connectorServerFactoryBean = new ConnectorServerFactoryBean();
+//        connectorServerFactoryBean.setObjectName("connector:name=rmi");
+//        connectorServerFactoryBean.setServiceUrl(String.format("service:jmx:rmi://%s:%s/jndi/rmi://%s:%s/jmxrmi", rmiHost, rmiPort, rmiHost, rmiPort));
+//        return connectorServerFactoryBean;
+//    }
 }
