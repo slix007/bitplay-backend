bb2538f5a293deb6bd2089742386ed6faa233cd5
Sergei Shurmin <slixsl007@gmail.com>
2019-09-03 12:08:21 +0400

    trade-delay-on-settlement fix changeset

--- core/src/main/java/com/bitplay/persistance/migration/changelogs/y2019/Changelog0830.java
+++ core/src/main/java/com/bitplay/persistance/migration/changelogs/y2019/Changelog0830.java
@@ -1,10 +1,10 @@
 package com.bitplay.persistance.migration.changelogs.y2019;
 
-import com.bitplay.persistance.domain.settings.OkexSettlement;
-import com.bitplay.persistance.domain.settings.Settings;
 import com.github.mongobee.changeset.ChangeLog;
 import com.github.mongobee.changeset.ChangeSet;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 
 /**
  * Created by Sergey Shurmin on 3/31/18.
@@ -14,8 +14,10 @@ public class Changelog0830 {
 
     @ChangeSet(order = "2019-08-30", id = "2019-08-30: Okex settlement", author = "SergeiShurmin")
     public void change01(MongoTemplate mongoTemplate) {
-        final Settings settings = mongoTemplate.findById(1L, Settings.class);
-        settings.setOkexSettlement(OkexSettlement.createDefault());
-        mongoTemplate.save(settings);
+        Query query = new Query();
+        Update update = new Update();
+        update.set("okexSettlement.active", false);
+        update.set("okexSettlement.period", 5);
+        mongoTemplate.updateMulti(query, update, "settingsCollection");
     }
 }
