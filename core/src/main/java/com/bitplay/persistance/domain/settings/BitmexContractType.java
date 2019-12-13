4384b7e42b80d9d5bbe0093b0cdec7ff578ce923
Sergei Shurmin <slixsl007@gmail.com>
2019-12-13 12:42:22 +0400

    bitmex new contractMode quarter - XBTH20

--- core/src/main/java/com/bitplay/persistance/domain/settings/BitmexContractType.java
+++ core/src/main/java/com/bitplay/persistance/domain/settings/BitmexContractType.java
@@ -16,13 +16,9 @@ public enum BitmexContractType implements ContractType {
     XBTU18(new CurrencyPair("XBT", "U18"), BigDecimal.valueOf(0.5), 1),
     XBTZ18(new CurrencyPair("XBT", "Z18"), BigDecimal.valueOf(0.5), 1),
     XBTZ19(new CurrencyPair("XBT", "Z19"), BigDecimal.valueOf(0.5), 1),
-    XBTU19(new CurrencyPair("XBT", "U19"), BigDecimal.valueOf(0.5), 1),
+    XBTH20(new CurrencyPair("XBT", "H20"), BigDecimal.valueOf(0.5), 1),
     ETHUSD(new CurrencyPair("ETH", "USD"), BigDecimal.valueOf(0.05), 2),
     ETHU18(new CurrencyPair("ETH", "U18"), BigDecimal.valueOf(0.05), 2),
-
-    // old
-    XBTH19(new CurrencyPair("XBT", "H19"), BigDecimal.valueOf(0.5), 1),
-
     ;
 
     private CurrencyPair currencyPair;
--- core/src/main/java/com/bitplay/persistance/domain/settings/ContractMode.java
+++ core/src/main/java/com/bitplay/persistance/domain/settings/ContractMode.java
@@ -8,8 +8,8 @@ import lombok.Getter;
 public enum ContractMode {
     MODE1_SET_BU11("M10", "set_bu11", BitmexContractType.XBTUSD, OkexContractType.BTC_ThisWeek),
     MODE2_SET_BU12("M11", "set_bu12", BitmexContractType.XBTUSD, OkexContractType.BTC_NextWeek),
-    MODE3_SET_BU23("M20", "set_bu23", BitmexContractType.XBTZ19, OkexContractType.BTC_Quarter),
-    MODE_TMP("TMP", "set_tmp", BitmexContractType.XBTU19, OkexContractType.BTC_Quarter),
+    MODE3_SET_BU23("M20", "set_bu23", BitmexContractType.XBTH20, OkexContractType.BTC_Quarter),
+    MODE_TMP("TMP", "set_tmp", BitmexContractType.XBTZ19, OkexContractType.BTC_NextWeek),// NextWeek!!!
     MODE4_SET_BU10_SET_EU11("M21", "set_eu11", BitmexContractType.ETHUSD, OkexContractType.ETH_ThisWeek),
     MODE5_SET_BU10_SET_EU12("M22", "set_eu12", BitmexContractType.ETHUSD, OkexContractType.ETH_NextWeek),
     BTC_SWAP_SET("BTC_SWAP", "BTC_SWAP", BitmexContractType.XBTUSD, OkexContractType.BTC_Swap),
