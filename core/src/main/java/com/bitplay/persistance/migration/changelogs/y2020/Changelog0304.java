a367e8852eb631c0ffd950b5e3a809ad7185f9aa
Sergei Shurmin <slixsl007@gmail.com>
2020-03-10 13:22:16 +0400

    1039-okex-okex - fix

--- core/src/main/java/com/bitplay/persistance/migration/changelogs/y2020/Changelog0304.java
+++ core/src/main/java/com/bitplay/persistance/migration/changelogs/y2020/Changelog0304.java
@@ -3,6 +3,7 @@ package com.bitplay.persistance.migration.changelogs.y2020;
 import com.bitplay.persistance.domain.settings.AllFtpd;
 import com.bitplay.persistance.domain.settings.AllPostOnlyArgs;
 import com.bitplay.persistance.domain.settings.Settings;
+import com.bitplay.persistance.domain.settings.UsdQuoteType;
 import com.github.mongobee.changeset.ChangeLog;
 import com.github.mongobee.changeset.ChangeSet;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -13,9 +14,10 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 @ChangeLog
 public class Changelog0304 {
 
-    @ChangeSet(order = "2020-03-03", id = "2020-03-03: usdQuoteType left-right..", author = "SergeiShurmin")
+    @ChangeSet(order = "2020-03-03", id = "2020-03-03: usdQuoteType left-right.Fix usdQuoteType", author = "SergeiShurmin")
     public void change01(MongoTemplate mongoTemplate) {
         final Settings settings = mongoTemplate.findById(1L, Settings.class);
+        settings.setUsdQuoteType(UsdQuoteType.LEFT);
         settings.setAllPostOnlyArgs(AllPostOnlyArgs.defaults());
         mongoTemplate.save(settings);
     }
