099f0811660b9f0b98da72621325035e29ecd390
Sergey Shurmin <sergey.shurmin@hoolah.co>
2022-04-03 14:43:08 +0400

    funding-sf fix timers

--- bitplay/src/main/java/com/bitplay/arbitrage/FundingTimerService.kt
+++ bitplay/src/main/java/com/bitplay/arbitrage/FundingTimerService.kt
@@ -28,11 +28,6 @@ class FundingTimerService(
     private val executor: ScheduledExecutorService =
         SchedulerUtils.fixedThreadExecutor("okex-funding-check-%d", 4)
 
-//    private val exLff = SchedulerUtils.singleThreadExecutor("exLff-%d")
-//    private val exLsf = SchedulerUtils.singleThreadExecutor("exLsf-%d")
-//    private val exRff = SchedulerUtils.singleThreadExecutor("exRff-%d")
-//    private val exRsf = SchedulerUtils.singleThreadExecutor("exRsf-%d")
-
     @Volatile
     private var futureLff: ScheduledFuture<*>? = null
 
@@ -49,15 +44,15 @@ class FundingTimerService(
     @EventListener(ArbitrageReadyEnableFundingEvent::class)
     fun init() {
         if (!arbitrageService.areBothOkex()) {
-            scheduleNextRunToFuture("leftFf")
-            scheduleNextRunToFuture("leftSf")
-            scheduleNextRunToFuture("rightFf")
-            scheduleNextRunToFuture("rightSf")
+            scheduleNextRunToFuture("leftFf", 8)
+            scheduleNextRunToFuture("leftSf", 16)
+            scheduleNextRunToFuture("rightFf", 8)
+            scheduleNextRunToFuture("rightSf", 16)
         }
     }
 
-    private fun scheduleNextRunToFuture(paramName: String) {
-        val future = scheduleNextRun(LocalTime.now(), paramName)
+    private fun scheduleNextRunToFuture(paramName: String, hoursForward: Int) {
+        val future = scheduleNextRun(paramName, hoursForward)
         when (paramName) {
             "leftFf" -> futureLff = future
             "leftSf" -> futureLsf = future
@@ -66,37 +61,66 @@ class FundingTimerService(
         }
     }
 
-    fun scheduleNextRun(currentTime: LocalTime, paramName: String): ScheduledFuture<*> {
+    fun scheduleNextRun(paramName: String, hoursForward: Int): ScheduledFuture<*> {
         val nextRunTime: LocalTime = settingsRepositoryService.settings
             .fundingSettings.getByParamName(paramName).getFundingTime()
 
-        var between = Duration.between(currentTime, nextRunTime).toKotlinDuration()
+        var between = Duration.between(LocalTime.now(), nextRunTime).toKotlinDuration()
         var timeTmp = nextRunTime
-        while ((between.isNegative() && between.absoluteValue.toDouble(DurationUnit.HOURS) < 16)
-            || between.toDouble(DurationUnit.HOURS) > 8
+        var iterations = 0;
+        val isFirst = hoursForward == 8
+        while (((isFirst && !isFirstByHours(between)) || (!isFirst && !isSecondByHours(between)))
+            && iterations++ < 5 // loop defence
         ) {
             timeTmp = timeTmp.plus(8, ChronoUnit.HOURS)
-            between = Duration.between(currentTime, timeTmp).toKotlinDuration()
+            between = Duration.between(LocalTime.now(), timeTmp).toKotlinDuration()
+            logger.info("time=$nextRunTime to timeTmp=$timeTmp")
         }
         if (nextRunTime != timeTmp) {
             settingsRepositoryService.updateFundingTime(
                 paramName,
-                timeTmp.format(DateTimeFormatter.ofPattern("HH:mm:ss"))
+                timeTmp.format(DateTimeFormatter.ofPattern("HH:mm:ss.SSS"))
             )
-            logger.info("time=$nextRunTime to timeTmp=$timeTmp")
         }
         logger.info("schedule nextRun at $nextRunTime to timeTmp=$timeTmp")
 
-        return executor.schedule({ nextRun(paramName) }, between.inWholeMilliseconds, TimeUnit.MILLISECONDS)
+        between = Duration.between(LocalTime.now(), timeTmp).toKotlinDuration().absoluteValue
+        return executor.schedule(
+            { nextRun(paramName, hoursForward) },
+            between.inWholeMilliseconds,
+            TimeUnit.MILLISECONDS
+        )
     }
 
-    fun nextRun(paramName: String) {
+    // 0..8..16..24
+    // 0..8........
+    fun isFirstByHours(between: kotlin.time.Duration) =
+        (between.isNegative()
+                && between.absoluteValue.toDouble(DurationUnit.HOURS) > 16
+                )
+                ||
+                (between.isPositive() && between.toDouble(DurationUnit.HOURS) < 8)
+
+    // 0..8..16..24
+    // ...8..16....
+    fun isSecondByHours(between: kotlin.time.Duration) =
+        (between.isNegative()
+                && between.absoluteValue.toDouble(DurationUnit.HOURS) < 16
+                && between.absoluteValue.toDouble(DurationUnit.HOURS) > 8
+                )
+                || (
+                between.isPositive()
+                        && between.absoluteValue.toDouble(DurationUnit.HOURS) > 8
+                        && between.absoluteValue.toDouble(DurationUnit.HOURS) < 16
+                )
+
+    fun nextRun(paramName: String, hoursForward: Int) {
         logger.info("FundingService run for $paramName")
 
         //TODO start the funding
 
 
-        scheduleNextRunToFuture(paramName)
+        scheduleNextRunToFuture(paramName, hoursForward)
     }
 
     fun isGreenTimeAll(): Boolean {
--- bitplay/src/test/java/com/bitplay/market/okcoin/OkexFundingServiceTest.kt
+++ bitplay/src/test/java/com/bitplay/market/okcoin/OkexFundingServiceTest.kt
@@ -3,11 +3,17 @@ package com.bitplay.market.okcoin
 import com.bitplay.arbitrage.ArbitrageService
 import com.bitplay.arbitrage.FundingTimerService
 import com.bitplay.persistance.SettingsRepositoryService
+import com.bitplay.persistance.domain.settings.FundingSettings
+import com.bitplay.persistance.domain.settings.Settings
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
 import org.mockito.Mockito
+import org.mockito.kotlin.whenever
 import org.springframework.context.ApplicationEvent
 import org.springframework.context.ApplicationEventPublisher
+import java.time.Duration
 import java.time.LocalTime
-import java.time.format.DateTimeFormatter
+import kotlin.time.toKotlinDuration
 
 class OkexFundingServiceTest {
 
@@ -23,17 +29,59 @@ class OkexFundingServiceTest {
             TODO("Not yet implemented")
         }
     }
-    private val fundingTimerService: FundingTimerService = FundingTimerService(applicationEventPublisher, srs, arbitrageService)
+    private val fundingTimerService: FundingTimerService =
+        FundingTimerService(applicationEventPublisher, srs, arbitrageService)
 
-    //    @Test
+    @Test
     fun nextRun() {
-        val now = LocalTime.now()
-        val formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-        println(now.format(formatter))
-        fundingTimerService.scheduleNextRun(
-            LocalTime.parse("13:36:00.897"),
-            ""
+        whenever(arbitrageService.areBothOkex()).thenReturn(false)
+        whenever(srs.settings).thenReturn(
+            Settings().apply {
+                fundingSettings = FundingSettings.createDefault()
+                fundingSettings.leftFf.scbSec = 0
+                fundingSettings.leftSf.scbSec = 24 * 60 * 60 + 1
+            }
         )
-//        Mockito.verify(srs.updateFundingSettings())
+        fundingTimerService.init()
+
+        assertThat(fundingTimerService.isGreenTime("leftFf")).isFalse
+        assertThat(fundingTimerService.isGreenTime("leftSf")).isTrue
+
+        println(fundingTimerService.getSecToRunLff())
+        println(fundingTimerService.getSecToRunLsf())
+    }
+
+    @Test
+    fun betweenTest() {
+        testOneTime("14:16")
+        testOneTime("20:16")
+        testOneTime("04:16")
+    }
+
+    private fun testOneTime(time: String) {
+        var b = Duration.between(LocalTime.parse(time), LocalTime.parse("20:00:00")).toKotlinDuration()
+        println(
+            "$time to 20:00:00=$b, \tf=${fundingTimerService.isFirstByHours(b)}, \ts= ${
+                fundingTimerService.isSecondByHours(
+                    b
+                )
+            }"
+        )
+        b = Duration.between(LocalTime.parse(time), LocalTime.parse("04:00:00")).toKotlinDuration()
+        println(
+            "$time to 04:00:00=$b, \tf=${fundingTimerService.isFirstByHours(b)}, \ts= ${
+                fundingTimerService.isSecondByHours(
+                    b
+                )
+            }"
+        )
+        b = Duration.between(LocalTime.parse(time), LocalTime.parse("12:00:00")).toKotlinDuration()
+        println(
+            "$time to 12:00:00=$b, \tf=${fundingTimerService.isFirstByHours(b)}, \ts= ${
+                fundingTimerService.isSecondByHours(
+                    b
+                )
+            }"
+        )
     }
 }
