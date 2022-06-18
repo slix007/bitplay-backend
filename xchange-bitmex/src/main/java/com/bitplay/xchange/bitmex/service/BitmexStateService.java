40b8dc770293b05d5ff4b37cf778be8601008ae8
Sergey Shurmin <sergey.shurmin@hoolah.co>
2022-06-18 20:40:28 +0400

    fix/1295-x-rate-limits-bitmex

--- xchange-bitmex/src/main/java/com/bitplay/xchange/bitmex/service/BitmexStateService.java
+++ xchange-bitmex/src/main/java/com/bitplay/xchange/bitmex/service/BitmexStateService.java
@@ -2,11 +2,12 @@ package com.bitplay.xchange.bitmex.service;
 
 import com.bitplay.xchange.bitmex.dto.BitmexXRateLimit;
 import com.bitplay.xchange.bitmex.dto.HeadersAware;
+import si.mazi.rescu.HttpStatusIOException;
+
 import java.time.Instant;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
-import si.mazi.rescu.HttpStatusIOException;
 
 public class BitmexStateService {
 
@@ -30,9 +31,21 @@ public class BitmexStateService {
     private void setXrateLimit(Map<String, List<String>> headers) {
         if (headers != null) {
 //            final List<String> limit = headers.get("x-ratelimit-limit");
-            final List<String> remaining = headers.get("x-ratelimit-remaining");
-            final List<String> remaining1s = headers.get("x-ratelimit-remaining-1s");
-            final List<String> reset = headers.get("x-ratelimit-reset");
+            final String remaining1 = "x-ratelimit-remaining";
+            final String remaining2 = "X-RateLimit-Remaining";
+            final String remaining1s1 = "x-ratelimit-remaining-1s";
+            final String remaining1s2 = "X-RateLimit-Remaining-1s";
+            final String reset1 = "x-ratelimit-reset";
+            final String reset2 = "X-RateLimit-Reset";
+            final List<String> remaining = headers.containsKey(remaining1)
+                    ? headers.get(remaining1)
+                    : headers.get(remaining2);
+            final List<String> remaining1s = headers.containsKey(remaining1)
+                    ? headers.get(remaining1s1)
+                    : headers.get(remaining1s2);
+            final List<String> reset = headers.containsKey(reset1)
+                    ? headers.get(reset1)
+                    : headers.get(reset2);
 
             final boolean hasRemaining = remaining != null && remaining.size() > 0;
             final boolean hasRemaining1s = remaining1s != null && remaining1s.size() > 0;
